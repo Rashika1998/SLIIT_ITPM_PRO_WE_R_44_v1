94c9c682f0a742930c1411cbb9ae93abeeb39de8
Rashika1998 <rashikamr1998@gmail.com>
2021-03-28 11:24:09 +0530

    Updated Table Headers.

--- SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.Designer.cs
+++ SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.Designer.cs
@@ -31,6 +31,13 @@ namespace SLIIT_ITPM_WE_R_44_V1
         {
             this.tableLayoutPanelGenerateTimeTable = new System.Windows.Forms.TableLayoutPanel();
             this.mondayLabel = new System.Windows.Forms.Label();
+            this.tuesdayLabel = new System.Windows.Forms.Label();
+            this.wednesdayLabel = new System.Windows.Forms.Label();
+            this.thursdayLabel = new System.Windows.Forms.Label();
+            this.fridayLabel = new System.Windows.Forms.Label();
+            this.saturdayLabel = new System.Windows.Forms.Label();
+            this.sundayLabel = new System.Windows.Forms.Label();
+            this.timeSlot = new System.Windows.Forms.Label();
             this.tableLayoutPanelGenerateTimeTable.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,32 +45,126 @@ namespace SLIIT_ITPM_WE_R_44_V1
             // 
             this.tableLayoutPanelGenerateTimeTable.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.tableLayoutPanelGenerateTimeTable.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
-            this.tableLayoutPanelGenerateTimeTable.ColumnCount = 7;
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.mondayLabel, 0, 0);
-            this.tableLayoutPanelGenerateTimeTable.Location = new System.Drawing.Point(96, 116);
+            this.tableLayoutPanelGenerateTimeTable.ColumnCount = 8;
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.timeSlot, 0, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.sundayLabel, 7, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.saturdayLabel, 6, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.fridayLabel, 5, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.thursdayLabel, 4, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.wednesdayLabel, 3, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.tuesdayLabel, 2, 0);
+            this.tableLayoutPanelGenerateTimeTable.Controls.Add(this.mondayLabel, 1, 0);
+            this.tableLayoutPanelGenerateTimeTable.Location = new System.Drawing.Point(82, 116);
             this.tableLayoutPanelGenerateTimeTable.Name = "tableLayoutPanelGenerateTimeTable";
             this.tableLayoutPanelGenerateTimeTable.RowCount = 2;
-            this.tableLayoutPanelGenerateTimeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelGenerateTimeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelGenerateTimeTable.Size = new System.Drawing.Size(784, 221);
+            this.tableLayoutPanelGenerateTimeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.09091F));
+            this.tableLayoutPanelGenerateTimeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80.90909F));
+            this.tableLayoutPanelGenerateTimeTable.Size = new System.Drawing.Size(867, 221);
             this.tableLayoutPanelGenerateTimeTable.TabIndex = 0;
             // 
             // mondayLabel
             // 
             this.mondayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.mondayLabel.AutoSize = true;
-            this.mondayLabel.Location = new System.Drawing.Point(41, 1);
+            this.mondayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mondayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.mondayLabel.Location = new System.Drawing.Point(128, 1);
             this.mondayLabel.Name = "mondayLabel";
-            this.mondayLabel.Size = new System.Drawing.Size(29, 13);
+            this.mondayLabel.Size = new System.Drawing.Size(68, 17);
             this.mondayLabel.TabIndex = 1;
-            this.mondayLabel.Text = "label";
+            this.mondayLabel.Text = "Monday";
+            // 
+            // tuesdayLabel
+            // 
+            this.tuesdayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tuesdayLabel.AutoSize = true;
+            this.tuesdayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tuesdayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.tuesdayLabel.Location = new System.Drawing.Point(233, 1);
+            this.tuesdayLabel.Name = "tuesdayLabel";
+            this.tuesdayLabel.Size = new System.Drawing.Size(75, 17);
+            this.tuesdayLabel.TabIndex = 2;
+            this.tuesdayLabel.Text = "Tuesday";
+            // 
+            // wednesdayLabel
+            // 
+            this.wednesdayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.wednesdayLabel.AutoSize = true;
+            this.wednesdayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wednesdayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.wednesdayLabel.Location = new System.Drawing.Point(330, 1);
+            this.wednesdayLabel.Name = "wednesdayLabel";
+            this.wednesdayLabel.Size = new System.Drawing.Size(97, 17);
+            this.wednesdayLabel.TabIndex = 3;
+            this.wednesdayLabel.Text = "Wednesday";
+            // 
+            // thursdayLabel
+            // 
+            this.thursdayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.thursdayLabel.AutoSize = true;
+            this.thursdayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thursdayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.thursdayLabel.Location = new System.Drawing.Point(445, 1);
+            this.thursdayLabel.Name = "thursdayLabel";
+            this.thursdayLabel.Size = new System.Drawing.Size(82, 17);
+            this.thursdayLabel.TabIndex = 4;
+            this.thursdayLabel.Text = "Thursday";
+            // 
+            // fridayLabel
+            // 
+            this.fridayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.fridayLabel.AutoSize = true;
+            this.fridayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fridayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.fridayLabel.Location = new System.Drawing.Point(566, 1);
+            this.fridayLabel.Name = "fridayLabel";
+            this.fridayLabel.Size = new System.Drawing.Size(57, 17);
+            this.fridayLabel.TabIndex = 5;
+            this.fridayLabel.Text = "Friday";
+            // 
+            // saturdayLabel
+            // 
+            this.saturdayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.saturdayLabel.AutoSize = true;
+            this.saturdayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saturdayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.saturdayLabel.Location = new System.Drawing.Point(664, 1);
+            this.saturdayLabel.Name = "saturdayLabel";
+            this.saturdayLabel.Size = new System.Drawing.Size(76, 17);
+            this.saturdayLabel.TabIndex = 6;
+            this.saturdayLabel.Text = "Saturday";
+            // 
+            // sundayLabel
+            // 
+            this.sundayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.sundayLabel.AutoSize = true;
+            this.sundayLabel.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sundayLabel.ForeColor = System.Drawing.Color.Gray;
+            this.sundayLabel.Location = new System.Drawing.Point(780, 1);
+            this.sundayLabel.Name = "sundayLabel";
+            this.sundayLabel.Size = new System.Drawing.Size(63, 17);
+            this.sundayLabel.TabIndex = 7;
+            this.sundayLabel.Text = "Sunday";
+            // 
+            // timeSlot
+            // 
+            this.timeSlot.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.timeSlot.AutoSize = true;
+            this.timeSlot.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeSlot.ForeColor = System.Drawing.Color.Gray;
+            this.timeSlot.Location = new System.Drawing.Point(15, 1);
+            this.timeSlot.Name = "timeSlot";
+            this.timeSlot.Size = new System.Drawing.Size(79, 17);
+            this.timeSlot.TabIndex = 8;
+            this.timeSlot.Text = "Time Slot";
             // 
             // GenerateTimeTables
             // 
@@ -85,5 +186,12 @@ namespace SLIIT_ITPM_WE_R_44_V1
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanelGenerateTimeTable;
         private System.Windows.Forms.Label mondayLabel;
+        private System.Windows.Forms.Label sundayLabel;
+        private System.Windows.Forms.Label saturdayLabel;
+        private System.Windows.Forms.Label fridayLabel;
+        private System.Windows.Forms.Label thursdayLabel;
+        private System.Windows.Forms.Label wednesdayLabel;
+        private System.Windows.Forms.Label tuesdayLabel;
+        private System.Windows.Forms.Label timeSlot;
     }
 }
--- SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.cs
+++ SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.cs
@@ -19,7 +19,7 @@ namespace SLIIT_ITPM_WE_R_44_V1
 
         private void GenerateTimeTables_Load(object sender, EventArgs e)
         {
-            mondayLabel.Text = "Monday";
+            
         }
     }
 }
