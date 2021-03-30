a1e39554e587b7695d49c7d8bd12f2a8ba3f3962
Rashika1998 <rashikamr1998@gmail.com>
2021-03-30 08:47:12 +0530

    Implementing Generate Time Tables feature.

--- SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.cs
+++ SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,12 @@ namespace SLIIT_ITPM_WE_R_44_V1
         {
             InitializeComponent();
         }
+
+
+
+        //SqlConnection con = new SqlConnection("Data Source=(local);Initial Catalog=ITPM_Y3S2_WE_R_44;User ID=sa;Password=rashika1998");
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database_we_r_44_v1.mdf;Integrated Security=True");
+
 
         //Get session details 
         //check starting time of the session
@@ -36,7 +43,46 @@ namespace SLIIT_ITPM_WE_R_44_V1
 
         public void getSessionDetails()
         {
+
+            String getStartingTime;
+
             //Get session details : Program, Lecturer 1 & 2, starting and ending time, Room
+
+            con.Open();
+            SqlCommand command1 = new SqlCommand("select Lecturer1 from AddSession", con);
+            SqlCommand command2 = new SqlCommand("select Lecturer2 from AddSession", con);
+            SqlCommand command3 = new SqlCommand("select Hrs from AddSession", con);
+            SqlCommand command4 = new SqlCommand("select Min from AddSession", con);
+            SqlCommand command5 = new SqlCommand("select Subject from AddSession", con);
+            SqlCommand command6 = new SqlCommand("select StudentGroup from AddSession", con);
+
+
+
+
+
+            try
+            {
+                con.Open();
+
+                using (SqlDataReader read = command3.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+
+                        //lecturer_name = (read["LecturerName"].ToString());
+                        //selectLecturercomboBox1.Items.Add(lecturer_name);
+                        //selectLecturercomboBox2.Items.Add(lecturer_name);
+
+
+                    }
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
         }
 
 
