4bbce76fa166694597ff6c9693c0429564bfc7cd
Rashika1998 <rashikamr1998@gmail.com>
2021-04-13 09:09:20 +0530

    Generating basic time table feature has been implemented. success...!

--- SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.cs
+++ SLIIT_ITPM_WE_R_44_V1/GenerateTimeTables.cs
@@ -20,6 +20,7 @@ namespace SLIIT_ITPM_WE_R_44_V1
 
 
 
+
         //SqlConnection con = new SqlConnection("Data Source=(local);Initial Catalog=ITPM_Y3S2_WE_R_44;User ID=sa;Password=rashika1998");
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database_we_r_44_v1.mdf;Integrated Security=True");
 
@@ -138,27 +139,273 @@ namespace SLIIT_ITPM_WE_R_44_V1
                         }
                         else if(getDay == "Tuesday")
                         {
+                            switch (getStartingTime)
+                            {
+                                case "8.30":
+                                    l10.Text = sessionDetails;
+                                    break;
 
+                                case "9.30":
+                                    l11.Text = sessionDetails;
+                                    break;
+
+                                case "10.30":
+                                    l12.Text = sessionDetails;
+                                    break;
+
+                                case "11.30":
+                                    l13.Text = sessionDetails;
+                                    break;
+
+                                case "12.30":
+                                    l14.Text = sessionDetails;
+                                    break;
+
+                                case "13.30":
+                                    l15.Text = sessionDetails;
+                                    break;
+
+                                case "14.30":
+                                    l16.Text = sessionDetails;
+                                    break;
+
+                                case "15.30":
+                                    l17.Text = sessionDetails;
+                                    break;
+
+                                case "16.30":
+                                    l18.Text = sessionDetails;
+                                    break;
+
+                                default:
+                                    // code block
+                                    break;
+                            }
                         }
                         else if(getDay == "Wednesday")
                         {
+                            switch (getStartingTime)
+                            {
+                                case "8.30":
+                                    l19.Text = sessionDetails;
+                                    break;
 
+                                case "9.30":
+                                    l20.Text = sessionDetails;
+                                    break;
+
+                                case "10.30":
+                                    l21.Text = sessionDetails;
+                                    break;
+
+                                case "11.30":
+                                    l22.Text = sessionDetails;
+                                    break;
+
+                                case "12.30":
+                                    l23.Text = sessionDetails;
+                                    break;
+
+                                case "13.30":
+                                    l24.Text = sessionDetails;
+                                    break;
+
+                                case "14.30":
+                                    l25.Text = sessionDetails;
+                                    break;
+
+                                case "15.30":
+                                    l26.Text = sessionDetails;
+                                    break;
+
+                                case "16.30":
+                                    l27.Text = sessionDetails;
+                                    break;
+
+                                default:
+                                    // code block
+                                    break;
+                            }
                         }
                         else if(getDay == "Thursday")
                         {
-                            
+                            switch (getStartingTime)
+                            {
+                                case "8.30":
+                                    l28.Text = sessionDetails;
+                                    break;
+
+                                case "9.30":
+                                    l29.Text = sessionDetails;
+                                    break;
+
+                                case "10.30":
+                                    l30.Text = sessionDetails;
+                                    break;
+
+                                case "11.30":
+                                    l31.Text = sessionDetails;
+                                    break;
+
+                                case "12.30":
+                                    l32.Text = sessionDetails;
+                                    break;
+
+                                case "13.30":
+                                    l33.Text = sessionDetails;
+                                    break;
+
+                                case "14.30":
+                                    l34.Text = sessionDetails;
+                                    break;
+
+                                case "15.30":
+                                    l35.Text = sessionDetails;
+                                    break;
+
+                                case "16.30":
+                                    l36.Text = sessionDetails;
+                                    break;
+
+                                default:
+                                    // code block
+                                    break;
+                            }
                         }
                         else if(getDay == "Friday")
                         {
+                            switch (getStartingTime)
+                            {
+                                case "8.30":
+                                    l37.Text = sessionDetails;
+                                    break;
 
+                                case "9.30":
+                                    l38.Text = sessionDetails;
+                                    break;
+
+                                case "10.30":
+                                    l39.Text = sessionDetails;
+                                    break;
+
+                                case "11.30":
+                                    l40.Text = sessionDetails;
+                                    break;
+
+                                case "12.30":
+                                    l41.Text = sessionDetails;
+                                    break;
+
+                                case "13.30":
+                                    l42.Text = sessionDetails;
+                                    break;
+
+                                case "14.30":
+                                    l43.Text = sessionDetails;
+                                    break;
+
+                                case "15.30":
+                                    l44.Text = sessionDetails;
+                                    break;
+
+                                case "16.30":
+                                    l45.Text = sessionDetails;
+                                    break;
+
+                                default:
+                                    // code block
+                                    break;
+                            }
                         }
                         else if(getDay == "Saturday")
                         {
+                            switch (getStartingTime)
+                            {
+                                case "8.30":
+                                    l46.Text = sessionDetails;
+                                    break;
 
+                                case "9.30":
+                                    l47.Text = sessionDetails;
+                                    break;
+
+                                case "10.30":
+                                    l48.Text = sessionDetails;
+                                    break;
+
+                                case "11.30":
+                                    l49.Text = sessionDetails;
+                                    break;
+
+                                case "12.30":
+                                    l50.Text = sessionDetails;
+                                    break;
+
+                                case "13.30":
+                                    l51.Text = sessionDetails;
+                                    break;
+
+                                case "14.30":
+                                    l52.Text = sessionDetails;
+                                    break;
+
+                                case "15.30":
+                                    l53.Text = sessionDetails;
+                                    break;
+
+                                case "16.30":
+                                    l54.Text = sessionDetails;
+                                    break;
+
+                                default:
+                                    // code block
+                                    break;
+                            }
                         }
                         else if(getDay == "Sunday")
                         {
+                            switch (getStartingTime)
+                            {
+                                case "8.30":
+                                    l55.Text = sessionDetails;
+                                    break;
 
+                                case "9.30":
+                                    l56.Text = sessionDetails;
+                                    break;
+
+                                case "10.30":
+                                    l57.Text = sessionDetails;
+                                    break;
+
+                                case "11.30":
+                                    l58.Text = sessionDetails;
+                                    break;
+
+                                case "12.30":
+                                    l59.Text = sessionDetails;
+                                    break;
+
+                                case "13.30":
+                                    l60.Text = sessionDetails;
+                                    break;
+
+                                case "14.30":
+                                    l61.Text = sessionDetails;
+                                    break;
+
+                                case "15.30":
+                                    l62.Text = sessionDetails;
+                                    break;
+
+                                case "16.30":
+                                    l63.Text = sessionDetails;
+                                    break;
+
+                                default:
+                                    // code block
+                                    break;
+                            }
                         }
                         else
                         {
