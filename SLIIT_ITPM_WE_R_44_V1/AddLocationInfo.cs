827a6430bf610d30392d0fe9b07cb7ad4e7acb11
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-18 23:14:54 +0530

    update addd location roomtype

--- SLIIT_ITPM_WE_R_44_V1/AddLocationInfo.cs
+++ SLIIT_ITPM_WE_R_44_V1/AddLocationInfo.cs
@@ -14,7 +14,19 @@ namespace SLIIT_ITPM_WE_R_44_V1
     public partial class AddLocationInfo : Form
     {
 
-        
+        public string roomType;
+
+
+        public AddLocationInfo()
+        {
+            InitializeComponent();
+        }
+
+
+        //SqlConnection con = new SqlConnection("Data Source=(local);Initial Catalog=ITPM_Y3S2_WE_R_44;User ID=sa;Password=rashika1998");
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database_we_r_44_v1.mdf;Integrated Security=True");
+
+
 
         public void cleanInputFields()
         {
