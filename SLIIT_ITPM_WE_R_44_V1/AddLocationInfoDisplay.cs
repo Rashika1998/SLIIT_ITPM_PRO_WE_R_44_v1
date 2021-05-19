1c7cf535c221418e60b06813591dca111d7c7709
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-19 23:35:56 +0530

    AddLocationInfoDisplay updated

--- SLIIT_ITPM_WE_R_44_V1/AddLocationInfoDisplay.cs
+++ SLIIT_ITPM_WE_R_44_V1/AddLocationInfoDisplay.cs
@@ -13,7 +13,11 @@ namespace SLIIT_ITPM_WE_R_44_V1
 {
     public partial class AddLocationInfoDisplay : Form
     {
-        
+        public AddLocationInfoDisplay()
+        {
+            InitializeComponent();
+        }
+
 
         //SqlConnection con = new SqlConnection("Data Source=(local);Initial Catalog=ITPM_Y3S2_WE_R_44;User ID=sa;Password=rashika1998");
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database_we_r_44_v1.mdf;Integrated Security=True");
